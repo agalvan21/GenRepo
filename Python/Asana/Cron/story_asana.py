dce036ae7856b3878c2f4eba0f6ef71cc16b4ff4
Alex <62806114+agalvan21@users.noreply.github.com>
2021-10-06 16:50:44 -0400

    Add files via upload

---
+++ Python/Asana/Cron/story_asana.py
@@ -0,0 +1,40 @@
+"""
+approval_asana.py 
+Created By: Alex Galvan
+Program will get last note and place it in a custom flield
+"""
+import asana
+import logging
+from pprint import pprint as pp
+logging.basicConfig(filename = 'asana_log.log',level = logging.INFO, format ='%(asctime)s:%(levelname)s:%(message)s')
+cred = '1/1111064222358483:98aad7b6d15fb22fb8283e174991fa14'
+client = asana.Client.access_token(cred)
+me = client.users.me()
+def get_tsk_stories(task_gid):
+    #returns last comments added resource_subtype': 'comment_added'
+    task_gid = task_gid
+    sub_text_type = list(client.stories.get_stories_for_task(task_gid))
+    comments_added = []
+    for comment in sub_text_type:
+        if comment['resource_subtype'] == 'comment_added':
+            comments_added.append(comment)
+    for comment in range(0,len(comments_added)):
+        all_comments = list(comments_added)
+    return(all_comments[-1]['text'])
+def get_inc_tsks():
+#Returns a list of all incompleted task gids called incompleted_tasks
+    project_gid = '1198513601388629'  
+    tasks_in_proj = list(client.tasks.get_tasks_for_project(project_gid, {'opt_fields': 'completed'}))
+    task_gid = []
+    for task in tasks_in_proj:
+        if task['completed'] == False:
+            task_gid.append(task['gid'])
+    return(task_gid)
+def main():
+    for tsk_gid in get_inc_tsks():
+        try:
+            client.tasks.update_task(tsk_gid,{'custom_fields':{'1201021503995606':get_tsk_stories(tsk_gid)}})
+        except:
+            pass
+main()
+
